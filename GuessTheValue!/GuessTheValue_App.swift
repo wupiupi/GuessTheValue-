06c3325351a5bab708f532f23647bd5326efc25a
Paul <pasha.makey@gmail.com>
2024-03-12 20:00:19 +0300

    Initial Commit

---
+++ GuessTheValue!/GuessTheValue_App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheValue_App.swift
+//  GuessTheValue!
+//
+//  Created by Paul Makey on 12.03.24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheValue_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
